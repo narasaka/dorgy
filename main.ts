fd3f17b488b9297c16a2f638602d0e91d3069f00
Nathanael Tehilla Gunawan <me@narasaka.dev>
2025-05-08 18:08:26 +0700

    refactor: udpate

--- main.ts
+++ main.ts
@@ -1,27 +1,27 @@
-import config from './config.json' assert { type: 'json' };
-import { ensureDir, path } from './deps.ts';
-import Logger from './logger.ts';
+import config from "./config.json" with { type: "json" };
+import { ensureDir, path } from "./deps.ts";
+import Logger from "./logger.ts";
 const { watchFs, copyFile, remove } = Deno;
 
 async function main() {
   const processing = new Set(); // keep track of on-going moves
   const watcher = watchFs(config.dirtyFolder);
-  const logger = Logger.getLogger()
+  const logger = Logger.getLogger();
 
   for await (const event of watcher) {
     logger.info(event);
     const { kind, paths } = event;
     const srcPath = path.normalize(paths[0]);
 
-    if (path.basename(srcPath) === '.DS_Store') continue;
+    if (path.basename(srcPath) === ".DS_Store") continue;
     if (processing.has(srcPath)) continue;
     processing.add(srcPath);
 
-    if (kind === 'create' || kind === 'modify') {
+    if (kind === "create" || kind === "modify") {
       const filename = path.basename(srcPath);
       const extname = path.extname(srcPath);
       const isStrayFile = extname.length === 0;
-      const destFolder = isStrayFile ? 'stray' : extname.substring(1);
+      const destFolder = isStrayFile ? "stray" : extname.substring(1);
       const destPath = path.join(config.cleanFolder, destFolder, filename);
       await ensureDir(path.join(config.cleanFolder, destFolder));
 
@@ -41,28 +41,28 @@ async function main() {
 }
 
 function isGood() {
-  if (config.cleanFolder === '') {
-    console.log('Please setup your clean folder.');
+  if (config.cleanFolder === "") {
+    console.log("Please setup your clean folder.");
     return false;
   }
-  if (config.dirtyFolder === '') {
-    console.log('Please setup your dirty folder.');
+  if (config.dirtyFolder === "") {
+    console.log("Please setup your dirty folder.");
     return false;
   }
   if (!path.isAbsolute(config.cleanFolder)) {
-    console.log('Please use absolute paths for your clean folder.');
+    console.log("Please use absolute paths for your clean folder.");
     return false;
   }
   if (!path.isAbsolute(config.dirtyFolder)) {
-    console.log('Please use absolute paths for your dirty folder.');
+    console.log("Please use absolute paths for your dirty folder.");
     return false;
   }
   if (config.cleanFolder === config.dirtyFolder) {
-    console.log('Dirty and clean folder cannot be the same.');
+    console.log("Dirty and clean folder cannot be the same.");
     return false;
   }
 
-  console.log('Checks completed. Running program...');
+  console.log("Checks completed. Running program...");
   return true;
 }
 
